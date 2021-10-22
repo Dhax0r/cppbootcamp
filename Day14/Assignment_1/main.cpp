00893489d0a2ff691093839d606c325f01eb3bc3
David Andersson <david.m.andersson@gmail.com>
2021-10-22 11:47:23 +0200

    use reinterpret_cast

--- Day14/Assignment_1/main.cpp
+++ Day14/Assignment_1/main.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 
 struct States {
-  unsigned seat_belts : 5;
-  unsigned windows : 4;
-  unsigned doors : 4;
   unsigned lights : 3;
+  unsigned doors : 4;
+  unsigned windows : 4;
+  unsigned seat_belts : 5;
+  
 };
 
 void function(uint16_t &);
@@ -18,36 +19,33 @@ int main() {
 }
 
 void function(uint16_t &input) {
-  States state;
-  state.lights = input;
-  state.doors = (input >> 3);
-  state.windows = (input >> 7);
-  state.seat_belts = (input >> 11);
+
+  States *state = reinterpret_cast<States*>(&input);
 
   for (size_t i = 1, j = 1; i <= 0x10; i <<= 1, j++){
     std::cout << "seat belt " << j  << " is ";
-    if (!(state.seat_belts & i)) {
+    if (!(state->seat_belts & i)) {
       std::cout << "not ";
     }
     std::cout << "buckled" << std::endl;
   }
-  for (size_t i = 1, j = 1; i <= 0x07; i <<= 1, j++){
+  for (size_t i = 1, j = 1; i <= 0x0F; i <<= 1, j++){
     std::cout << "window " << j  << " is ";
-    if (!(state.windows & i)) {
+    if (!(state->windows & i)) {
       std::cout << "not ";
     }
     std::cout << "open" << std::endl;
   }
-  for (size_t i = 1, j = 1; i <= 0x07; i <<= 1, j++){
+  for (size_t i = 1, j = 1; i <= 0x0F; i <<= 1, j++){
     std::cout << "door " << j  << " is ";
-    if (!(state.doors & i)) {
+    if (!(state->doors & i)) {
       std::cout << "not ";
     }
     std::cout << "open" << std::endl;
   }
   for (size_t i = 1, j = 1; i <= 0x07; i <<= 1, j++){
     std::cout << "light " << j  << " is ";
-    if (!(state.lights & i)) {
+    if (!(state->lights & i)) {
       std::cout << "not ";
     }
     std::cout << "on" << std::endl;
