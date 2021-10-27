4ad4962bd704adbf9f8928e1de95e00520a4454d
David Andersson <david.m.andersson@gmail.com>
2021-10-27 16:22:51 +0200

    reorder functions

--- mini_project/eliminator.cpp
+++ mini_project/eliminator.cpp
@@ -100,19 +100,7 @@ bool EliminateInRow(const int &value, const int &row,
   return true;
 }
 
-bool UniqueInRow(const int &value, const size_t &row, const size_t &col, Grid_t inner_state) {
-  for (size_t i = 0; i < 9; i++) {
-    if (i == col) {
-      continue;
-    }
-    for (auto p : inner_state[row][i].possible) {
-      if (p == value || inner_state[row][i].value == value) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
+
 bool EliminateInCol(const int &value, const int &col,
                     Grid_t &inner_state) {
   for (size_t i = 0; i < inner_state.size(); i++) {
@@ -133,37 +121,9 @@ bool EliminateInCol(const int &value, const int &col,
   }
   return true;
 }
-bool UniqueInCol(const int &value, const size_t &row, const size_t &col, const Grid_t &inner_state) {
-  for (size_t i = 0; i < 9; i++) {
-    if (i == row) {
-      continue;
-    }
-    for (auto p : inner_state[i][col].possible) {
-      if (p == value) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-void UniquePeer(Grid_t &inner_state) {
-for (size_t i = 0; i < 81; i++) {
-    if (inner_state[i/9][i%9].value != -1){
-      continue;
-    }
-    for (auto p : inner_state[i/9][i%9].possible) {
-      if (UniqueInRow(p, i/9, i%9, inner_state)) {
-        AssignValue(i/9, i%9, p, inner_state);
-      }
-      if (UniqueInCol(p, i/9, i%9, inner_state)) {
-        AssignValue(i/9, i%9, p, inner_state);
-      }
-      if (UniqueInBox(p, i/9, i%9, inner_state)) {
-        AssignValue(i/9, i%9, p, inner_state);
-      }
-    }
-  }
-}
+
+
+//Eliminate value in box
 bool EliminateInBox(const int &value, const int &row, const int &col,
                     Grid_t &inner_state) {
   int box_row = row - row % 3;
@@ -189,7 +149,51 @@ bool EliminateInBox(const int &value, const int &row, const int &col,
   }
   return true;
 }
-
+// Go through grid and check if any peers are unique
+void UniquePeer(Grid_t &inner_state) {
+for (size_t i = 0; i < 81; i++) {
+    if (inner_state[i/9][i%9].value != -1){
+      continue;
+    }
+    for (auto p : inner_state[i/9][i%9].possible) {
+      if (UniqueInRow(p, i/9, i%9, inner_state)) {
+        AssignValue(i/9, i%9, p, inner_state);
+      }
+      if (UniqueInCol(p, i/9, i%9, inner_state)) {
+        AssignValue(i/9, i%9, p, inner_state);
+      }
+      if (UniqueInBox(p, i/9, i%9, inner_state)) {
+        AssignValue(i/9, i%9, p, inner_state);
+      }
+    }
+  }
+}
+bool UniqueInRow(const int &value, const size_t &row, const size_t &col, Grid_t inner_state) {
+  for (size_t i = 0; i < 9; i++) {
+    if (i == col) {
+      continue;
+    }
+    for (auto p : inner_state[row][i].possible) {
+      if (p == value || inner_state[row][i].value == value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+bool UniqueInCol(const int &value, const size_t &row, const size_t &col, const Grid_t &inner_state) {
+  for (size_t i = 0; i < 9; i++) {
+    if (i == row) {
+      continue;
+    }
+    for (auto p : inner_state[i][col].possible) {
+      if (p == value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 bool UniqueInBox(const int &value, const size_t &row, const size_t &col, const Grid_t &inner_state) {
   size_t box_row = row - row % 3;
   size_t box_col = col - col % 3;
@@ -225,7 +229,12 @@ void FindMinPeers(size_t &min_row, size_t &min_col,
     }
   }
 }
-
+bool IsValid(const size_t &row, const size_t &col, const int &value,
+             const Grid_t &inner_state) {
+  return IsValidRow(row, value, inner_state) &&
+         IsValidCol(col, value, inner_state) &&
+         IsValidBox(row, col, value, inner_state);
+}
 bool IsValidRow(const size_t &row, const int &value,
                 const Grid_t &inner_state) {
   for (size_t i = 0; i < inner_state[0].size(); i++) {
@@ -260,9 +269,3 @@ bool IsValidBox(const size_t &row, const size_t &col, const int &value,
   return true;
 }
 
-bool IsValid(const size_t &row, const size_t &col, const int &value,
-             const Grid_t &inner_state) {
-  return IsValidRow(row, value, inner_state) &&
-         IsValidCol(col, value, inner_state) &&
-         IsValidBox(row, col, value, inner_state);
-}
